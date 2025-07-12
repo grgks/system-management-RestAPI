823d6f7539f0690e49deb78a3b4726d399409732
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-07-12 23:35:20 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/system_management_restAPI/SystemManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.system_management_restAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemManagementRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/system_management_restAPI/SystemManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.system_management_restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
