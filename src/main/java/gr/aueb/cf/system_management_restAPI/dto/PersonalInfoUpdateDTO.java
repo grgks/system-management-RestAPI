c80f4b29f975f6951f627c41ced19a5213714adb
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-07-13 00:48:57 +0300

    feat: add PersonalInfoUpdateDTO

---
+++ src/main/java/gr/aueb/cf/system_management_restAPI/dto/PersonalInfoUpdateDTO.java
@@ -0,0 +1,39 @@
+package gr.aueb.cf.system_management_restAPI.dto;
+
+import gr.aueb.cf.system_management_restAPI.core.enums.GenderType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonalInfoUpdateDTO {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be exactly 10 digits")
+    private String phone;
+
+    private LocalDate dateOfBirth;
+
+    private GenderType gender;
+
+    private String address;
+
+    private Long cityId;
+}
