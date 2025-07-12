9b9de446905c22dad22c272ba1296bfd9188dcc2
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-07-12 23:50:16 +0300

    feat:add Client

---
+++ src/main/java/gr/aueb/cf/system_management_restAPI/model/Client.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.system_management_restAPI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import java.util.UUID;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+public class Client extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "uuid", unique = true, nullable = false)
+    private UUID uuid;
+
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "personal_info_id")
+    private PersonalInfo personalInfo;
+
+    @Column(name = "vat", unique = true)
+    private String vat;
+
+    @Column(name = "notes")
+    private String notes;
+}
