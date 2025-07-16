84a2c5735831ec50a5cfb5fe3d81a18cf2761726
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-07-16 18:21:57 +0300

    feat: add JWTService

---
+++ src/main/java/gr/aueb/cf/system_management_restAPI/security/JwtService.java
@@ -0,0 +1,88 @@
+package gr.aueb.cf.system_management_restAPI.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+
+    private String secretKey = "29c35697495ce6fc7770610ea221ca1145367c726b0a2b17800bc41c1c65150c";
+    private long jwtExpiration = 7200000;  // 2 hours in milliseconds
+
+    public String generateToken(String username, String role) {
+        var claims = new HashMap<String, Object>();
+        claims.put("role", role);
+        return Jwts
+                .builder()
+                .setIssuer("system-management-api")
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String subject = extractSubject(token);
+        return (subject.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public String getStringClaim(String token, String claim) {
+        return extractAllClaims(token).get(claim, String.class);
+    }
+
+    public String extractSubject(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public long getExpirationTime() {
+        return jwtExpiration;
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * Creates a HS256 Key. Key is an interface.
+     * Starting from secretKey we get a byte array
+     * of the secret. Then we get the {@link javax.crypto.SecretKey,
+     * class that implements the {@link Key } interface.
+     *
+     * @return  a SecretKey which implements Key.
+     */
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
