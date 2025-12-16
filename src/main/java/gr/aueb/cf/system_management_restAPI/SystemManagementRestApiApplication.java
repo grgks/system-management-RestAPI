90d67d640ea217d11164f7ddeded49795a01fb8a
KOYN <gkeo933@gmail.com>
2025-12-16 21:43:01 +0200

    feat(config): enable async processing for background tasks

--- src/main/java/gr/aueb/cf/system_management_restAPI/SystemManagementRestApiApplication.java
+++ src/main/java/gr/aueb/cf/system_management_restAPI/SystemManagementRestApiApplication.java
@@ -3,9 +3,11 @@ package gr.aueb.cf.system_management_restAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class SystemManagementRestApiApplication {
 
 	public static void main(String[] args) {
